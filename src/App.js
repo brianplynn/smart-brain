346943674dccc6c3d630cba3a33a7fda9c603c0e
brianplynn <brnlynn889@gmail.com>
2018-10-19 18:53:24 -0700

    updating back-end

--- src/App.js
+++ src/App.js
@@ -1,25 +1,154 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Navigation from "./components/Navigation.js";
+import Logo from "./components/Logo.js";
+import ImageLinkForm from "./components/ImageLinkForm.js";
+import FaceRecognition from "./components/FaceRecognition.js";
+import Rank from "./components/Rank.js"
+import Particles from 'react-particles-js';
+import Signin from "./components/Signin.js";
+import Register from "./components/Register.js";
+
+/*
+https://samples.clarifai.com/metro-north.jpg
+https://samples.clarifai.com/face-det.jpg
+
+*/
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 150,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
+
+const initialState = {
+      input: "",
+      imageUrl: "",
+      box: {},
+      route: "sign-in",
+      isSignedIn: false,
+      user: {
+        id: "",
+        name: '',
+        email: '',
+        entries: 0,
+        joined: '',
+      }
+}
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: "",
+      imageUrl: "",
+      box: {},
+      route: "sign-in",
+      isSignedIn: false,
+      user: {
+        id: "",
+        name: '',
+        email: '',
+        entries: 0,
+        joined: '',
+      }
+    }
+  }
+
+  loadUser= (data) => {
+    this.setState({user: {
+        id: data.id,
+        name: data.name,
+        email: data.name,
+        entries: data.entries,
+        joined: data.joined,
+    }});
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById("inputImage");
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row*height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    this.setState({box: box})
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value})
+  }
+
+  onSubmit = (event) => {
+    this.setState( {imageUrl: this.state.input})
+    fetch("https://obscure-savannah-71554.herokuapp.com/imageurl", {
+            method: "post",
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+              input: this.state.input
+            })
+          })
+      .then(response => response.json())
+      .then(response => {
+        if (response) {
+          fetch("https://obscure-savannah-71554.herokuapp.com/image", {
+            method: "put",
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+              id: this.state.user.id
+            })
+          })
+            .then(response => response.json())
+            .then(count => this.setState(Object.assign(this.state.user, { entries: count })))
+            .catch(err => console.log(err));
+        }
+        this.displayFaceBox(this.calculateFaceLocation(response))
+      })
+      .catch(err => console.log(err));
+  }
+
+  onRouteChange = (route) => {
+    if (route === "sign-out") {
+      this.setState(initialState)
+    } else if (route === "home") {
+      this.setState({ isSignedIn: true })
+    } 
+    this.setState({ route: route });
+  }
+
   render() {
+    const { isSignedIn, route, box, imageUrl } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Particles className="particles" params={particlesOptions}/>
+        <Navigation isSignedIn = {isSignedIn} onRouteChange={this.onRouteChange}/>
+        {route === "home" 
+        ? <div>
+            <Logo />
+            <Rank name={this.state.user.name} 
+            entries={this.state.user.entries}/>
+            <ImageLinkForm 
+              onSubmit={this.onSubmit} 
+              onInputChange={this.onInputChange} />
+            <FaceRecognition box={box} imageUrl = {imageUrl}/>
+          </div> 
+          : (route === "sign-in" || route === "sign-out"
+          ? <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
+          : <Register loadUser={this.loadUser} 
+             onRouteChange={this.onRouteChange} />)
+        }
       </div>
     );
   }
